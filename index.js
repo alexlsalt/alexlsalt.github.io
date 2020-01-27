8d54977f51bea4b9acc8d250d9ad289f6b759f2f
Alex Morton <am@alexmortoncreative.com>
2020-01-27 14:06:41 +0100

    Cleaned up JS file for better readability and documentation

--- index.js
+++ index.js
@@ -1,41 +1,36 @@
-let projectsButton = document.getElementById('projects-button');
-
-//Project button global variables
+// Display Travel Go Budget project details
 
 const buttonOne = document.getElementById('button-one');
 const descrOne = document.getElementById('first-description');
 
-//Project at placement two recently removed
+function showOne() {
+  descrOne.style.display = 'block';
+  buttonOne.style.display = 'none';
+  };
+
+  buttonOne.onclick = showOne;
+  
+
+
+
+// Display Port Townsend Eats project details
 
 const buttonThree = document.getElementById('button-three');
 const descrThree = document.getElementById('third-description');
 
-const buttonFour = document.getElementById('button-four');
-const descrFour = document.getElementById('fourth-description');
-
-//First example element
-const showOne = () => {
-descrOne.style.display = 'block';
-buttonOne.style.display = 'none';
-};
-
-  buttonOne.onclick = showOne;
-
-
-//Second example element recently removed
-
-//Third example element
-
-const showThree = () => {
+function showThree() {
   descrThree.style.display = 'block';
   buttonThree.style.display = 'none';
 };
 
 buttonThree.onclick = showThree;
 
-//Fourth example element
+// Display Mahalo project details
 
-const showFour = () => {
+const buttonFour = document.getElementById('button-four');
+const descrFour = document.getElementById('fourth-description');
+
+function showFour() {
   descrFour.style.display = 'block';
   buttonFour.style.display = 'none';
 };
