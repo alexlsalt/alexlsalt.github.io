8a3ba265939445ba16bcef35f301343938da81a5
Alex Morton <am@alexmortoncreative.com>
2020-09-14 14:01:45 +0200

    Updated script.js for project information buttons

--- script.js
+++ script.js
@@ -34,17 +34,6 @@ function showFive() {
 
   button5.onclick = showFive;
 
-// Display Travel Go Budget project details
-
-const buttonOne = document.getElementById('button-one');
-const descrOne = document.getElementById('first-description');
-
-function showOne() {
-  descrOne.style.display = 'block';
-  buttonOne.style.display = 'none';
-  };
-
-  buttonOne.onclick = showOne;
 
   // Display Three To-Do's A Day project details
 
@@ -59,7 +48,7 @@ function showTwo() {
   buttonTwo.onclick = showTwo;
 
 
-// Display Port Townsend Eats project details
+// Display Travel Go Budget project details
 
 const buttonThree = document.getElementById('button-three');
 const descrThree = document.getElementById('third-description');
@@ -71,15 +60,15 @@ function showThree() {
 
 buttonThree.onclick = showThree;
 
-// Display Mahalo project details
+// Display Port Townsend Eats project details
 
-const buttonFour = document.getElementById('button-four');
-const descrFour = document.getElementById('fourth-description');
+// const buttonFour = document.getElementById('button-four');
+// const descrFour = document.getElementById('fourth-description');
 
-function showFour() {
-  descrFour.style.display = 'block';
-  buttonFour.style.display = 'none';
-};
+// function showFour() {
+//   descrFour.style.display = 'block';
+//   buttonFour.style.display = 'none';
+// };
 
-buttonFour.onclick = showFour;
+// buttonFour.onclick = showFour;
 
