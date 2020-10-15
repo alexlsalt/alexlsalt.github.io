706355dcf625a34469b1c37fb4b9c7cd6ea3589c
Alex Morton <am@alexmortoncreative.com>
2020-10-15 17:36:08 +0200

    Refactored displayProjectInfo logic to just a single function

--- script.js
+++ script.js
@@ -1,86 +1,27 @@
+function displayProjectInfo(str) {
+  const button = document.querySelector(`#button-${str}`);
+  const description = document.querySelector(`#description-${str}`);
+
+  button.addEventListener('click', () => {
+    description.style.display = 'block';
+    button.style.display = 'none';
+  });
+}
+
+// SYTYCF project details
+displayProjectInfo('eight');
+
 // Ladies Code Collective project details
-
-const button8 = document.getElementById('button-eight');
-const descr8 = document.getElementById('eighth-description');
-
-function showEight() {
-  descr8.style.display = 'block';
-  button8.style.display = 'none';
-  };
-
-  button8.onclick = showEight;
-
-// Ladies Code Collective project details
-
-const button7 = document.getElementById('button-seven');
-const descr7 = document.getElementById('seventh-description');
-
-function showSeven() {
-  descr7.style.display = 'block';
-  button7.style.display = 'none';
-  };
-
-  button7.onclick = showSeven;
+displayProjectInfo('seven');
 
 // Wellbean project details
-
-const button6 = document.getElementById('button-six');
-const descr6 = document.getElementById('sixth-description');
-
-function showSix() {
-  descr6.style.display = 'block';
-  button6.style.display = 'none';
-  };
-
-  button6.onclick = showSix;
+displayProjectInfo('six');
 
 // Letters from Nelson project details
+displayProjectInfo('five');
 
-const button5 = document.getElementById('button-five');
-const descr5 = document.getElementById('fifth-description');
-
-function showFive() {
-  descr5.style.display = 'block';
-  button5.style.display = 'none';
-  };
-
-  button5.onclick = showFive;
-
-
-  // Display Three To-Do's A Day project details
-
-const buttonTwo = document.getElementById('button-two');
-const descrTwo = document.getElementById('second-description');
-
-function showTwo() {
-  descrTwo.style.display = 'block';
-  buttonTwo.style.display = 'none';
-  };
-
-  buttonTwo.onclick = showTwo;
-
+// Display Three To-Do's A Day project details
+displayProjectInfo('two');
 
 // Display Travel Go Budget project details
-
-const buttonThree = document.getElementById('button-three');
-const descrThree = document.getElementById('third-description');
-
-function showThree() {
-  descrThree.style.display = 'block';
-  buttonThree.style.display = 'none';
-};
-
-buttonThree.onclick = showThree;
-
-// Display Port Townsend Eats project details
-
-// const buttonFour = document.getElementById('button-four');
-// const descrFour = document.getElementById('fourth-description');
-
-// function showFour() {
-//   descrFour.style.display = 'block';
-//   buttonFour.style.display = 'none';
-// };
-
-// buttonFour.onclick = showFour;
-
+displayProjectInfo('three');
